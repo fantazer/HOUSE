b1b95c462d2ddd4d352780368e7bfdcb83d7c50c
FANTAZER <kuznecovs45@gmail.com>
2018-02-18 22:00:31 +0300

    add modal

--- app/js/script.js
+++ app/js/script.js
@@ -12,10 +12,162 @@
 $(document).ready(function(){
 
 	// template scrollbar
-	$('.price__list').perfectScrollbar()
+	$('.price__list').perfectScrollbar();
 	// template scrollbar === end
 
+	//modals
+	$('.modal-content').click(function(event){
+		event.stopPropagation();
+	});
+	var scrollPos = 0;
+
+	var openModal = function () {
+	if(!$('.modal-layer').hasClass('modal-layer-show')){
+		$('.modal-layer').addClass('modal-layer-show');
+	}
+	 scrollPos = $(window).scrollTop();
+		$('body').css({
+			overflow: 'hidden',
+			position: 'fixed',
+			overflowY: 'scroll',
+			top : -scrollPos,
+			width:'100%'
+		});
+		return scrollPos;
+	};
+
+	var closeModal = function () {
+		console.log("scrollPos",scrollPos);
+  	$('.modal-layer').removeClass('modal-layer-show');
+  	$("body").removeClass("modal-fix");
+  	$('body').css({
+			overflow: '',
+			position: '',
+			top: ''
+		})
+    $(window).scrollTop(scrollPos);
+    $('.modal').removeClass('modal__show');
+		$('.enter').removeClass('enter--open');
+		$('.basket').removeClass('basket--open');
+	};
+
+	var initModal = function(el){
+		openModal();
+		$('.enter').removeClass('enter--open');
+		$('.basket').removeClass('basket--open');
+		$('.modal').each(function () {
+			if ($(this).data('modal')===el){
+				$(this).addClass('modal__show')
+			} else {
+				$(this).removeClass('modal__show')
+			}
+		});
+		var modalHeightCont = $(window).height();
+		$('.modal-filter').height(modalHeightCont);
+		$('.modal-wrap').css('height',modalHeightCont );
+		$('.modal-wrap').css('minHeight',modalHeightCont );
+	}
+
+	$('.modal-get').click(function (){
+		var currentModal = $(this).data("modal");
+		initModal(currentModal);
+	});
+
+	$('.modal-layer , .modal-close').click(function (){
+		closeModal();
+	});
+	//modals ===end
+
+	//validate
+	$('.validate-form').each(function() {
+		var curentForm = $(this);
+    $(this).validate({
+    			highlight: function(element) { //даем родителю класс если есть ошибка
+							$(element).parent().addClass("field-error");
+					},
+					unhighlight: function(element) {
+							$(element).parent().removeClass("field-error");
+					},
+		    	rules:{ //правила для полей
+						name:{
+							required:true,
+						},
+						phone:{
+							required:true,
+							minlength:5,
+							number:true
+						},
+						comment:{
+							required:true,
+							minlength:5,
+						},
+						agree: {
+							required: true
+						}
+					},
+					messages:{
+						name:{
+							required: 'Обязательное поле',
+						},
+						phone:{
+							required: 'Обязательное поле',
+							number:'Введите правильный номер',
+							minlength:'Номер должен быть длиннее',
+						},
+						comment:{
+							required: 'Обязательное поле',
+							minlength:'Сообщение должно быть длиннее',
+						},
+						agree:{
+							required: false,
+						}
+					},
+					submitHandler : function(form){
+						$.ajax({ //отправка ajax
+						            type: "POST",
+						            url: "/wp-content/themes/AAK/sender.php",
+						            data: $(form).serialize(),
+						            timeout: 3000,
+						          });
+							closeModal();
+							initModal("next");
+							setTimeout(function(){
+										closeModal();
+										$(':input','.validate-form') //очитска формы от данных
+										  .not(':button, :submit, :reset, :hidden')
+										  .val('')
+										  .removeAttr('checked')
+										  .removeAttr('selected')
+							},2500)
+
+				}
+		    });
+		});
+	//validate===end
+
+
+	//fancy-box
 	$(".fancybox").fancybox();
+	//fancy-box === end
+
+	//scroll top
+	var scrollTop = 750;
+	$(window).scroll(function() {
+		var scroll = $(this).scrollTop();
+		if ( scroll >= scrollTop ) {
+				//var heightHeader=$('.header-main-wrap').height();
+				$('.scroll-up').addClass('scroll-up--show');
+			}
+			else {
+					$('.scroll-up').removeClass('scroll-up--show');
+			}
+	});
+
+	$('.scroll-up').click(function() {
+    $('html, body').animate({scrollTop: 0},800);
+    return false;
+  });
+	//scroll top ===end
 
 	$('.scheme-slider').slick({
 		slidesToShow: 4,
